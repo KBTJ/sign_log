a5368fb55cb5b3714bca14af3ce29c11a0fd1e58
KBTJ <briankatinega839@gmail.com>
2021-11-14 17:00:05 +0300

    Database files fix

--- database/config.php
+++ database/config.php
@@ -4,4 +4,3 @@ $dbUsername = "root";
 $dbPassword = "";
 $dbName = "Learner";
  ?>
-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
--- database/connection.php
+++ database/connection.php
@@ -1,12 +1,13 @@
+<?php include 'config.php'?>
 <?php
 $conn = mysqli_connect($dbServerName , $dbUsername , $dbPassword , $dbName );
 
 if (!$conn)
           {
-          die("Connection Failed:" .mysqli_connect_error());
+          die("Connection Failed:".mysqli_connect_error());
           }
  else
     {
-      echo "Connection Successful";
+      // echo "Connection Successful";
     }
  ?>
